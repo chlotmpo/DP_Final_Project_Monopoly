d3e169a3250d99b6a7ea58eecca532f77622408c
Chloé TEMPO <76529865+chlotmpo@users.noreply.github.com>
2021-12-24 18:24:28 +0100

    debut de code des classes property, tax, jail, neutral, chance, community chest et creation du plateau de jeu

--- MonopolyGame/MonopolyGame/Tax.cs
+++ MonopolyGame/MonopolyGame/Tax.cs
@@ -8,21 +8,43 @@ namespace MonopolyGame
 {
     public class Tax : ICell
     {
+        // ATTRIBUTES 
         // TODO : define all the attributes that we need to correclty instantiate a tax cell
-        public double taxAmount;
+        private double taxAmount;
+        private string name;
         private int position;
 
+        // CONSTRUCTORS
         public Tax() { }
-        public Tax(int position)
+        public Tax(string name, double taxAmount, int position)
         {
+            this.name = name;
+            this.taxAmount = taxAmount;
             this.position = position;
         }
 
+        // PROPERTIES
+        public double TaxAmount
+        {
+            get { return taxAmount; }
+        }
+        public string Name
+        {
+            get { return name; }
+        }
         public int Position
         {
             get { return position; }
-            set { this.position = value; }
         }
 
+        // METHODS
+        /// <summary>
+        /// Method that give a description and informations of the current tax cell
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "Cell position : " + position + "\nYou are on a tex cell. That means that you will have to pay an amount of money to the bank.\nPay now $" + taxAmount + ".";
+        }
     }
 }
